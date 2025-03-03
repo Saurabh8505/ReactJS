871c95110438251c333e33fb03387a87ecee453b
Saurabh Mishra <103993107+Saurabh8505@users.noreply.github.com>
2025-03-03 19:32:05 +0530

    added Simple-form-handling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,13 +7,13 @@ function App() {
     const increment = () => setCount(count + 1);
     const decrement = () => setCount(count - 1);
 
-    return ( <
-        div >
+    return ( 
+        <div>
 
-        <
-        h1 > Counter: { count } < /h1> <
-        button onClick = { increment } > Increment < /button>  <
-        button onClick = { decrement } > Decrement < /button> </div >
+        <h1> Counter: { count } </h1> 
+        <button onClick = { increment } > Increment </button>  
+        <button onClick = { decrement } > Decrement </button> 
+        </div >
 
     );
 }
--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,34 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function App() {
-    const [tasks, setTasks] = useState([]);
-    const [newTask, setNewTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
 
-    const handleAddTask = () => {
-        setTasks([...tasks, newTask]);
-        setNewTask('');
-    };
+  const handleAddTask = () => {
+    setTasks([...tasks, newTask]);
+    setNewTask("");
+  };
 
-    return ( <
-        div >
-        <
-        h1 > To - Do List < /h1> <
-        input type = "text"
-        value = { newTask }
-        onChange = {
-            (e) => setNewTask(e.target.value) }
-        placeholder = "Add a new task" /
-        >
-        <
-        button onClick = { handleAddTask } > Add Task < /button> <
-        ul > {
-            tasks.map((task, index) => ( <
-                li key = { index } > { task } < /li>
-            ))
-        } <
-        /ul> <
-        /div>
-    );
+  return (
+    <div>
+      <h1> To - Do List </h1>{" "}
+      <input
+        type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        placeholder="Add a new task"
+      />
+      <button onClick={handleAddTask}> Add Task </button>{" "}
+      <ul>
+        {" "}
+        {tasks.map((task, index) => (
+          <li key={index}> {task} </li>
+        ))}{" "}
+      </ul>{" "}
+    </div>
+  );
 }
 
 export default App;
